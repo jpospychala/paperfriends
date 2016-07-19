92c5c44f24a862a3cbeccb83375352460a0c7ba2
Jacek Pospychala <jacek.pospychala@gmail.com>
2016-07-20 00:19:29 +0200

    app: phoenix app from template

---
+++ app/website/config/config.exs
@@ -0,0 +1,23 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# Configures the endpoint
+config :website, Website.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "MFk3857+bRWE7M1aPJm5qCT6F/QsFQKCw8Gz0XfWYbv4pnmzgCtTs7u+AdYW+S5A",
+  render_errors: [view: Website.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Website.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
