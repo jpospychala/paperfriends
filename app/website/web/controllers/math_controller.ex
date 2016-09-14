2933989e23a9ddefc213c7d08c09d221b5935a9b
Jacek Pospychala <jacek.pospychala@gmail.com>
2016-09-14 22:16:43 +0200

    remove unused files

--- app/website/web/controllers/math_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule Website.MathController do
-  use Website.Web, :controller
-
-  def sum(conn, %{"a" => a, "b" => b}) do
-    json conn, %{"sum" => String.to_integer(a) + String.to_integer(b) }
-  end
-end
