7f60d5a056f314377a635f66aea5d265fde16721
Jacek Pospychala <jacek.pospychala@gmail.com>
2016-09-19 00:31:11 +0200

    take model id from url

--- app/website/web/static/js/app.js
+++ app/website/web/static/js/app.js
@@ -10,13 +10,13 @@ sync.start(socket, state);
 var vm = new Vue({
   el: "#scene",
   data: {
-    message: "Paperfriends",
+    model_id: +getParameterByName("model"),
     editable: "",
     model: {},
     error: undefined
   },
   ready: function () {
-    $.getJSON('/api/models/1', (response) => {
+    $.getJSON(`/api/models/${this.model_id}`, (response) => {
       this.model = response.data;
       this.editable = JSON.stringify(this.model.body, true, 2);
 
@@ -39,14 +39,23 @@ var vm = new Vue({
       this.applyChanges();
       $.ajax({
         type: "PUT",
-        url: "/api/models/1",
+        url: `/api/models/${this.model_id}`,
         data: JSON.stringify({model: this.model}),
-        success: (response) => {
-          console.log(response);
-        },
+        success: (response) => {},
+        error: function(response) {this.error = response; },
         contentType: "application/json",
         dataType: "json"
       });
     }
   }
 });
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
