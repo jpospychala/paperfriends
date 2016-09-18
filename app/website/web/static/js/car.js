a19758975b585fb449e4f91c3e889c0eca8e9c0e
Jacek Pospychala <jacek.pospychala@gmail.com>
2016-09-18 23:44:35 +0200

    split model definition into parts

--- app/website/web/static/js/car.js
+++ app/website/web/static/js/car.js
@@ -33,32 +33,81 @@ let rect = function(dx, dy, z) {
 
 function Car(model) {
   this.model = model;
-  this.outline = svgPathToPoints(this.model.outlinePoints);
 
   var materials = new Materials();
 
   this.mesh = function() {
-    var shape = pointsToShape(this.outline);
+    var group = new THREE.Group();
+    this.model.parts.forEach(part => {
+      var partMesh = shapes[part.type](part, materials);
+      group.add(partMesh);
+    });
+
+    var center = new THREE.Group();
+    group.updateMatrixWorld();
+    var boundingBox = maxBoundingBox(group, new THREE.Box3());
+    console.log(boundingBox);
+    var xOfs = Math.abs(boundingBox.max.x-boundingBox.min.x)/2;
+    var zOfs = Math.abs(boundingBox.max.z-boundingBox.min.z)/2;
+    console.log(xOfs, zOfs);
+    group.translateX(-xOfs);
+    group.translateZ(zOfs);
+    center.add(group);
+
+    return center;
+  };
+}
+
+function maxBoundingBox (obj, box) {
+  if (obj.geometry) {
+    if (!obj.geometry.boundingBox) {
+      obj.geometry.computeBoundingBox();
+    }
+    var trsf = obj.geometry.boundingBox.clone();
+    obj.localToWorld(trsf);
+    box.union(trsf);
+  }
+
+  if (obj.children) {
+    obj.children.forEach(child => maxBoundingBox(child, box));
+  }
+  return box;
+}
+
+const shapes = {
+  wheel: function(wheel, materials) {
+    var geometry = new THREE.CircleGeometry(wheel.r, 32);
+    var material = materials.get(wheel.face);
+    var circle = new THREE.Mesh(geometry, material);
+    circle.translateX(wheel.x);
+    circle.translateY(wheel.y);
+    circle.translateZ(wheel.z);
+    return circle;
+  },
+  car: function(car, materials) {
+    var outline = svgPathToPoints(car.outlinePoints);
+    var shape = pointsToShape(outline);
     var geometry = shape.makeGeometry();
     rescaleUvs(geometry);
 
-    var group = new THREE.Group();
+    materials.setDefaultColor(car.color);
 
-    materials.setDefaultColor(this.model.color);
-    var lmesh = new THREE.Mesh(geometry, materials.get(this.model.faces[0]));
+    var group = new THREE.Group();
+    var lmesh = new THREE.Mesh(geometry, materials.get(car.faces[0]));
     group.add(lmesh);
 
-    var rmesh = new THREE.Mesh(geometry, materials.get(this.model.faces[1]));
-    rmesh.position.z = -this.model.width;
+    var rmesh = new THREE.Mesh(geometry, materials.get(car.faces[1]));
+    rmesh.position.z = -car.width;
     group.add(rmesh);
+
     var front = new THREE.Group();
     front.rotateY(PI/2);
-    for (var i = 0; i < this.outline.length; i++) {
-      var p1 = this.outline[i];
-      var sideShape = rect(p1.dx, p1.dy, this.model.width);
+    for (var i = 0; i < outline.length; i++) {
+      var p1 = outline[i];
+      var sideShape = rect(p1.dx, p1.dy, car.width);
       var shapeGeometry = sideShape.makeGeometry();
       rescaleUvs(shapeGeometry);
-      var sideMesh = new THREE.Mesh(shapeGeometry, materials.get(this.model.faces[i + 2]));
+      var sideMesh = new THREE.Mesh(shapeGeometry, materials.get(car.faces[i + 2]));
       sideMesh.position.set(0, p1.y, p1.x);
 
       var upright = p1.dx >= 0 && p1.dy < 0 ? -1 : 0;
@@ -70,24 +119,8 @@ function Car(model) {
       front.add(sideMesh);
     }
     group.add(front);
-    group.translateX(-geometry.boundingBox.max.x/2);
-    group.translateZ(this.model.width/2);
-
-    this.model.wheels.forEach(wheel => {
-      var geometry = new THREE.CircleGeometry(wheel.r, 32);
-      var material = materials.get(wheel.face);
-      var circle = new THREE.Mesh(geometry, material);
-      circle.translateX(wheel.x);
-      circle.translateY(wheel.y);
-      circle.translateZ(wheel.z);
-      group.add(circle);
-    });
-
-    var center = new THREE.Group();
-    center.add(group);
-
-    return center;
-  };
+    return group;
+  }
 }
 
 let rescaleUvs = geometry => {
